4d2891bdef21a8adad816d1895042ca966337086
Valentina Corak <valentinacorak@gmail.com>
2021-06-22 11:36:38 +0200

    Student controller

--- Controller/StudentController.php
+++ Controller/StudentController.php
@@ -1,1 +1,19 @@
 <?php
+declare(strict_types = 1);
+
+class StudentController
+{
+    //render function with both $_GET and $_POST vars available if it would be needed.
+    public function render(array $GET, array $POST)
+    {
+
+        // $loader is array filled with objects
+        $loader = new StudentLoader();
+        $allStudents = $loader->getStudents();
+        var_dump($allStudents);
+
+
+
+        require 'View/student.php';
+    }
+}
